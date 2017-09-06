7c2d2cabb206240c53728f2a36991fccc24dc49c
Mark Smith (nl) <mark_smith_nl@yahoo.co.uk>
2017-09-06 23:45:26 +0200

    Refactor 3

--- src/main/java/nl/smith/primenumber/NumberGenerator.java
+++ src/main/java/nl/smith/primenumber/NumberGenerator.java
@@ -15,14 +15,29 @@ public class NumberGenerator {
 
     private BigDecimal ceiling;
 
+    /**
+     * 
+     * @param current
+     * @param offset
+     * @param ceiling
+     */
     public NumberGenerator(BigDecimal current, BigDecimal offset, BigDecimal ceiling) {
+        if (current == null || offset == null || ceiling == null) {
+            throw new IllegalArgumentException("Null values for current, offset and ceiling are not allowed.");
+        }
+        if (current.compareTo(ceiling) == 1) {
+            throw new IllegalArgumentException("Values for current larger then ceiling are not allowed.");
+        }
+        if (offset.compareTo(BigDecimal.ONE) == -1) {
+            throw new IllegalArgumentException("Values for offset smaller then one are not allowed.");
+        }
         this.current = current;
         this.offset = offset;
         this.ceiling = ceiling;
     }
 
     public synchronized BigDecimal getNumber() {
-        if (current.compareTo(ceiling) > 1) {
+        if (current.compareTo(ceiling) == 1) {
             return null;
         }
 
@@ -30,6 +45,18 @@ public class NumberGenerator {
         current = current.add(offset);
 
         return number;
+    }
+
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator(new BigDecimal(10), new BigDecimal(3), new BigDecimal(20));
+        BigDecimal current;
+        do {
+            current = numberGenerator.getNumber();
+            System.out.println(current);
+        } while (current != null);
+
+        System.out.println("Einde main");
+
     }
 
 }
